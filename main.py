dda34aff528253df906fa7af8a07b2c684f75c92
Coko <91132775+Coko7@users.noreply.github.com>
2024-06-01 18:58:34 +0200

    feat: add AFK fisher script

---
+++ main.py
@@ -0,0 +1,71 @@
+import cv2
+import numpy as np
+import pyautogui
+import time
+import keyboard
+
+
+def check_catch(x_start, x_end, y_start, y_end):
+    screenshot = np.array(pyautogui.screenshot())
+
+    # Crop the region of interest
+    roi = screenshot[y_start:y_end, x_start:x_end]
+
+    # Convert the region of interest to grayscale
+    grayscale_roi = cv2.cvtColor(roi, cv2.COLOR_BGR2GRAY)
+
+    # Define the threshold for detecting black pixels
+    threshold = 10  # Adjust this value as needed
+
+    # Check if there is a black square area
+    if np.mean(grayscale_roi) < threshold:
+        x, y = pyautogui.position()
+        pyautogui.rightClick(0, 0)
+        time.sleep(3.5)
+        pyautogui.rightClick(0, 0)
+        return 1
+
+    return 0
+    # cv2.imwrite("test.png", grayscale_roi)
+
+
+# Get screen dimensions
+screen_width, screen_height = pyautogui.size()
+
+# Screen is 2560x1440
+big_screen_roi = {'x1': 2375, 'x2': 2385, 'y1': 1285, 'y2': 1342}
+
+# Screen is 1920x1080
+laptop_screen_roi = {'x1': 1725, 'x2': 1745, 'y1': 930, 'y2': 985}
+
+# roi_width = int(screen_width * 0.1)  # 20% of the screen width
+# roi_height = int(screen_height * 0.1)  # 20% of the screen height
+#
+# roi_x = screen_width - roi_width
+# roi_y = screen_height - roi_height
+
+roi = laptop_screen_roi
+
+print("5...")
+time.sleep(1)
+print("4...")
+time.sleep(1)
+print("3...")
+time.sleep(1)
+print("2...")
+time.sleep(1)
+print("1...")
+time.sleep(1)
+print("GO! GO! GO! GOOOOOO!!!")
+
+catches = 0
+while True:
+    is_catch = check_catch(roi['x1'], roi['x2'], roi['y1'], roi['y2'])
+    if is_catch:
+        catches = catches + 1
+        print("Catch:", catches)
+
+    if keyboard.is_pressed("p"):
+        print("IT'S G-OVER")
+        print("You caught:", catches)
+        break
